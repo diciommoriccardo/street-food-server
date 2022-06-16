699864c1e53b10edadcf3006f242ae238748caba
diciommoriccardo <riccardo.diciommo1@gmail.com>
2022-06-16 13:22:31 +0200

    Controllers

---
+++ src/controllers/Orders.controller.js
@@ -0,0 +1,38 @@
+import { ParamsMissing } from "../helpers/Errors.js";
+import Orders from "../models/Orders.js";
+
+const orderController = {
+    getById: (req) => {
+        return new Promise((resolve, reject) => {
+            if(!req.params._id || req.params._id == '') reject(new ParamsMissing("_id"))
+
+            Orders.findById(req.params._id)
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+        })
+    },
+
+    create: (req) => {
+        return new Promise((resolve, reject) => {
+            new Orders(req.body).save()
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+        })
+    },
+
+    updateOne: (req) => {
+        return new Promise((resolve, reject) => {
+            Orders.findByIdAndUpdate(req.body._id, req.body).save()
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+        })
+    },
+
+    update: (req) => {
+        return new Promise((resolve, reject) => {
+            //idk
+        })
+    }
+}
+
+export default orderController;
---
+++ src/controllers/Products.controller.js
@@ -0,0 +1,40 @@
+import Products from '../models/Products.js';
+import { ParamsMissing } from '../helpers/Errors.js';
+
+const productsController = {
+    getById: (req) => {
+        return new Promise((resolve, reject) => {
+            if(!req.params._id || req.params._id == '') reject(new ParamsMissing("_id"))
+
+            Products.findById(req.params._id)
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+        }) 
+    },
+
+    create: (req) => {
+        return new Promise((resolve, reject) => {
+            new Products(req.body).save()
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+        })
+    },
+
+    getAll: (req) => {
+        return new Promise((resolve, reject) => {
+            Products.find().skip(req.body.offset).limit(req.body.limit)
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+        })
+    },
+
+    updateOne: (req) => {
+        return new Promise((resolve, reject) => {
+            Products.findOneAndUpdate(req.body._id, req.body)
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+        })
+    }
+}
+
+export default productsController;
